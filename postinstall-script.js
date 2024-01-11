4d61bd6fd8117ada233836e30a61287c3d3419bc
Amal P Sojan <amalpsojan@hotmail.com>
2024-01-12 01:07:58 +0530

    fix:updated

--- postinstall-script.js
+++ postinstall-script.js
@@ -27,11 +27,9 @@ const path = require("path");
 const projectParentDirectory = path.resolve(__dirname, "${_projectParentDirectory}");
 const sharedLibDirectory = path.join(projectParentDirectory, "${sharedLibDirectoryName}" + "/packages");
 
-
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
-
 
 config.watchFolders = [sharedLibDirectory];
 
@@ -46,47 +44,60 @@ module.exports = config;`;
 //key = directory name; value = package name
 const linkPackages = { "mobile-theme": "@amalpsojan/mobile-theme" };
 
-try {
-  // Check if the lib directory exists
-  if (!fs.existsSync(sharedLibDirectory)) {
-    // Clone the monorepo project
-    execSync(`git clone ${gitRepo} ` + sharedLibDirectory);
+function cloneMonorepo() {
+  execSync(`git clone ${gitRepo} ` + sharedLibDirectory);
+}
+
+function linkPackage(packagePath, value) {
+  execSync(`cd ${packagePath} && yarn link`);
+  execSync(`cd ${projectDirectory} && yarn link ${value}`);
+  console.log(`${value} successfully linked`);
+}
+
+function createFile(filePath, fileContent) {
+  fs.writeFileSync(filePath, fileContent);
+  console.log(`${path.basename(filePath)} file created successfully!`);
+}
+
+function checkDirectoryExists(directory) {
+  return fs.existsSync(directory);
+}
+
+function checkMonorepoAccess() {
+  try {
+    execSync("git ls-remote --exit-code " + gitRepo);
+    console.log("User has access to the monorepo. No need to clone, directory already exists.");
 
     for (const id in linkPackages) {
       const key = id;
       const value = linkPackages[id];
-
       const packagePath = `${sharedLibDirectory}/packages/${key}`;
 
-      if (fs.existsSync(packagePath)) {
-        execSync(`cd ${packagePath} && yarn link`);
-        execSync(`cd ${projectDirectory} && yarn link ${value}`);
-        console.log(`${value} successfully linked`);
+      if (checkDirectoryExists(packagePath)) {
+        linkPackage(packagePath, value);
       }
     }
 
-    console.log("Monorepo project cloned and packages linked successfully!");
-    fs.writeFileSync(indexFilePath, indexFileContent);
-    console.log("index.js file created successfully!");
+    console.log("Packages linked successfully!");
+    createFile(indexFilePath, indexFileContent);
+    createFile(metroConfigFilePath, metroConfigContent);
 
-    fs.writeFileSync(metroConfigFilePath, metroConfigContent);
-    console.log("metro.config.js file created successfully!");
+    console.warn("Run Expo Cache clearing Script before running (clear)");
+  } catch (error) {
+    console.error("User does not have access to the monorepo. Please check your permissions.");
+  }
+}
 
-    console.warn("Run Expo Cache clearing Script before running (clear)")
+try {
+  // Check if the lib directory exists
+  if (!checkDirectoryExists(sharedLibDirectory)) {
+    // Clone the monorepo project
+    cloneMonorepo();
+    checkMonorepoAccess();
   } else {
     // Check if the user has access to the monorepo
     // You can replace the URL with your actual monorepo URL
-
-    try {
-      execSync("git ls-remote --exit-code " + gitRepo);
-      console.log(
-        "User has access to the monorepo. No need to clone, directory already exists."
-      );
-    } catch (error) {
-      console.error(
-        "User does not have access to the monorepo. Please check your permissions."
-      );
-    }
+    checkMonorepoAccess();
   }
 } catch (error) {
   console.error("Error during postinstall script execution:", error.message);
